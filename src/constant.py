38e1d4a497e7daf4c4a4077b21ffd9f634ba5467
Kenji Hikmatullah <iniemailkenji@gmail.com>
2022-05-15 13:47:11 +0700

    Create src dir

--- src/constant.py
+++ src/constant.py
@@ -1,12 +1,12 @@
 INPUT_FILE = 'input/its_water_distribution.inp'
 
-EPANET_JAR_FILE = r"C:\Users\kenji\OneDrive - Institut Teknologi Sepuluh Nopember\Thesis\EPANET\water-flow-forecasting\AwareEpanetNoDeps.jar"
+EPANET_JAR_FILE = r"AwareEpanetNoDeps.jar"
 
 INITIAL_ROUGHNESS = 150  # Initial roughness value
 STEP = 50  # Added roughness value per-step
 STEP_COUNT = 5  # Number of steps
 
-OUTPUT_DIR = 'output'
+OUTPUT_DIR = 'output/'
 
-OUTPUT_ROUGHNESS_DIR = 'output/roughness'
+OUTPUT_ROUGHNESS_DIR = 'output/roughness/'
 OUTPUT_ROUGHNESS_FILE = 'water_flow_forecast.csv'
